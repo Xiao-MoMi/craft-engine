816f9b3fa02a20ea3e5aad885ce4b3b6cc3c220d
XiaoMoMi <littlemomi@qq.com>
2026-02-12 12:40:56 +0800

    提升shadow版本

--- bukkit/loader/build.gradle.kts
+++ bukkit/loader/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.gradleup.shadow") version "9.3.0"
+    id("com.gradleup.shadow") version "9.3.1"
     id("de.eldoria.plugin-yml.bukkit") version "0.7.1"
 }
 
--- bukkit/paper-loader/build.gradle.kts
+++ bukkit/paper-loader/build.gradle.kts
@@ -2,7 +2,7 @@ import net.minecrell.pluginyml.paper.PaperPluginDescription
 import xyz.jpenilla.runpaper.task.RunServer
 
 plugins {
-    id("com.gradleup.shadow") version "9.3.0"
+    id("com.gradleup.shadow") version "9.3.1"
     id("de.eldoria.plugin-yml.paper") version "0.7.1"
     id("xyz.jpenilla.run-paper") version "3.0.2"
 }
