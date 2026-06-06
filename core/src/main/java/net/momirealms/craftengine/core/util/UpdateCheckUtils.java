f30618d5f6c0919dcc4cf6987a1dc3ac91096e4e
jhqwqmc <jhcraft@qq.com>
2026-06-06 08:38:10 +0800

    改用更好的请求接口

--- core/src/main/java/net/momirealms/craftengine/core/util/UpdateCheckUtils.java
+++ core/src/main/java/net/momirealms/craftengine/core/util/UpdateCheckUtils.java
@@ -1,22 +1,25 @@
 package net.momirealms.craftengine.core.util;
 
 import com.google.gson.JsonObject;
-import com.google.gson.reflect.TypeToken;
 import net.momirealms.craftengine.core.plugin.CraftEngine;
 import net.momirealms.craftengine.core.plugin.locale.TranslationManager;
 import org.jetbrains.annotations.Nullable;
+import org.jspecify.annotations.NonNull;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.List;
+import java.nio.charset.StandardCharsets;
 import java.util.Locale;
 
 public final class UpdateCheckUtils {
-    private static final String MODRINTH_API_URL = "https://api.modrinth.com/v2/project/craftengine/version";
-    private static final String MODRINTH_CN_API_URL = "https://mod.mcimirror.top/modrinth/v2/project/craftengine/version";
+    private static final String MODRINTH_API_URL = "https://api.modrinth.com/v2/version_files/update";
+    private static final String MODRINTH_CN_API_URL = "https://mod.mcimirror.top/modrinth/v2/version_files/update";
 
     private UpdateCheckUtils() {}
 
@@ -72,12 +75,7 @@ public final class UpdateCheckUtils {
 
     @Nullable
     private static String getLatestVersion(String apiUrl) throws Exception {
-        URL url = new URI(apiUrl).toURL();
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("GET");
-        connection.setRequestProperty("User-Agent", "Mozilla/5.0");
-        connection.setConnectTimeout(5000);
-        connection.setReadTimeout(5000);
+        HttpURLConnection connection = getConnection(apiUrl);
         int responseCode = connection.getResponseCode();
         if (responseCode == HttpURLConnection.HTTP_OK) {
             BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
@@ -87,16 +85,33 @@ public final class UpdateCheckUtils {
                 response.append(inputLine);
             }
             in.close();
-            List<JsonObject> versions = GsonHelper.get().fromJson(
-                    response.toString(),
-                    new TypeToken<List<JsonObject>>(){}.getType()
-            );
-            return versions.stream()
-                    .map(v -> v.get("version_number").getAsString())
-                    .max(UpdateCheckUtils::compareVer)
-                    .orElse(null);
+            return GsonHelper.get().fromJson(response.toString(), JsonObject.class)
+                    .asMap().values().iterator().next()
+                    .getAsJsonObject()
+                    .get("version_number")
+                    .getAsString();
         }
         return null;
+    }
+
+    private static @NonNull HttpURLConnection getConnection(String apiUrl) throws URISyntaxException, IOException {
+        URL url = new URI(apiUrl).toURL();
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("User-Agent", "Mozilla/5.0");
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setDoOutput(true);
+        connection.setConnectTimeout(5000);
+        connection.setReadTimeout(5000);
+        String requestBody = "{\"algorithm\":\"sha512\",\"game_versions\":[],\"hashes\":[\"" +
+                // 这里使用 26.6 的 sha512 来获取最新版本
+                "357da69552ef525b790d3f09cf2c9782b5642cc4bae7fbdb50aaecbc675035154c913771e0736a596a523033cd74fc8934dd14014d3683780f36e15c0e5d2c73" +
+                "\"],\"loaders\":[]}";
+        try (OutputStream os = connection.getOutputStream()) {
+            byte[] input = requestBody.getBytes(StandardCharsets.UTF_8);
+            os.write(input, 0, input.length);
+        }
+        return connection;
     }
 
     private static String[] getUpdateUrls() {
