064e4e48e1b6c464820af608eb242115a8f2097b
XiaoMoMi <70987828+Xiao-MoMi@users.noreply.github.com>
2026-09-14 03:01:55 +0800

    修复错误注解

--- bukkit/proxy/src/main/java/net/momirealms/craftengine/proxy/minecraft/world/level/chunk/PalettedContainerProxy.java
+++ bukkit/proxy/src/main/java/net/momirealms/craftengine/proxy/minecraft/world/level/chunk/PalettedContainerProxy.java
@@ -35,7 +35,7 @@ public interface PalettedContainerProxy {
         return null;
     }
 
-    @MethodInvoker(name = "leaf$getDataAcquire", activeIf = "has_patch=leaf && min_version=26.2", optional = true)
+    @MethodInvoker(name = "leaf$getFromData", activeIf = "has_patch=leaf && min_version=26.2", optional = true)
     default Object leaf$getFromData(Object target, @Type(clazz = PalettedContainerProxy.DataProxy.class) Object data, int index) {
         return null;
     }
